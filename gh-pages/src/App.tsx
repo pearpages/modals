a6026919c05998c2c9f312a383f11773c12b4bb9
pearpages <pere@soms.cat>
2025-09-14 10:21:22 +0200

    fix: gh-pages

--- gh-pages/src/App.tsx
+++ gh-pages/src/App.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@pearpages/modals";
+import { ModalExample } from "@pearpages/modals";
 import json from "../../package.json";
 
 function App() {
   return (
     <>
       <h1>Contribution Modals v{json.version}</h1>
-      <Button />
+      <ModalExample />
     </>
   );
 }
